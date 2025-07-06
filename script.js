94dd8b88a4866159850dedacb8e54ad7a89a73ab
Priyanshu Pareek <165381369+StillWater2005@users.noreply.github.com>
2025-07-06 19:58:03 +0530

    Added pluto to the simulation.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
-const distances = [1, 1.85, 2.56, 3.90, 13.33, 24.56, 49.28, 77.05];
-const mercury_rad = 5;
-const delay = 100;
+const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto"];
+const distances = [1, 1.85, 2.56, 3.90, 13.33, 24.56, 49.28, 77.05, 101.28];
+const mercury_rad = 4;
+const delay = 25;
 const radius_list = distances.map((element) => {
     return element * mercury_rad;
 })
 const planet_node = planets.map((element) =>{
     return document.querySelector(`.${element}`);
 })
-let planet_x = [0, 0, 0, 0, 0, 0, 0, 0];
-let planet_y = [0, 0, 0, 0, 0, 0, 0, 0];
+let planet_x = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let planet_y = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function set_planet(planet_node, x, y)
 {
@@ -18,7 +18,7 @@ function set_planet(planet_node, x, y)
 }
 
 let time = 0;
-const rel_periods = [1, 2.58, 4.17, 7.83, 49.42, 122.75, 350, 686.67];
+const rel_periods = [1, 2.58, 4.17, 7.83, 49.42, 122.75, 350, 686.67, 1033.33];
 const mercury_period = 100;
 const periods = rel_periods.map((element) => {
     return element * mercury_period;
@@ -30,7 +30,7 @@ function angle_rad(period, time)//takes in time period and time and return angle
 }
 
 function update(){
-    for(let i = 0; i < 8; i++)
+    for(let i = 0; i < 9; i++)
     {
         let angle = angle_rad(periods[i], time);
         planet_x[i] = radius_list[i] * Math.cos(angle);
@@ -39,7 +39,7 @@ function update(){
 }
 
 function paint(){
-    for(let i = 0; i < 8; i++)
+    for(let i = 0; i < 9; i++)
     {
         set_planet(planet_node[i], planet_x[i], planet_y[i]);
     }
